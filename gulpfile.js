ada2ceee36da196d22dfb7e4f03f6d6944100ff9
Johan Dercksen <johan@draco.co.za>
2018-09-26 19:13:42 +0200

    Published Version 0.0.7-beta of the package(s)

--- gulpfile.js
+++ gulpfile.js
@@ -3,15 +3,22 @@ let clc = require('cli-color');
 let filter = require('gulp-filter');
 let path = require('path');
 let yaml = require('yamljs');
+let del = require('gulp-clean');
 
 let {clean, restore, build, test, pack, publish, push, run} = require('gulp-dotnet-cli');
 
-let buildDir      = process.cwd();
-let buildSettings = { };
+let buildDir          = process.cwd();
+let nugetDir          = path.join(buildDir, '/buildoutput/nuget');
+let nugetPublishedDir = path.join(buildDir, '/buildoutput/nugetPublished');
+let buildSettings     = { };
 
 gulp.task('load-settings', function(done) {
     console.log(clc.blueBright('Loading Build Settings'));
     buildSettings = yaml.load('./.buildSettings.yml');
+
+    console.log('Version      :', buildSettings.VERSION);
+    console.log('Configuration:', buildSettings.CONFIGURATION);
+    console.log('Build Dir    :', buildDir);
 
     done();
 });
@@ -115,7 +122,7 @@ gulp.task('publish-win10', gulp.series('test', (done) => {
 }));
 
 // Create NuGet package from existinbg project(s)
-gulp.task('pack', gulp.series('load-settings', 'test', (done) => {
+gulp.task('nuget-pack', gulp.series('load-settings', 'test', (done) => {
     console.log(clc.blueBright('Starting Execution of Pack - Creation of nuget packages'));
 
     const testFilter = filter(['src/**/*.csproj', '!src/**/*Tests.csproj'], { restore: true })
@@ -131,7 +138,7 @@ gulp.task('pack', gulp.series('load-settings', 'test', (done) => {
             configuration: buildSettings.CONFIGURATION,
             version: nugetVersion,
             noBuild: true,
-            includeSymbols: true,
+            includeSymbols: false,
             output: path.join(buildDir, '/buildoutput/nuget'),
             runtime: 'netstandard2.0'
         }))
@@ -146,12 +153,10 @@ gulp.task('pack', gulp.series('load-settings', 'test', (done) => {
 }));
 
 // Push nuget packages to a server
-gulp.task('push', gulp.series('pack', (done) => {
+gulp.task('nuget-publish', gulp.series('nuget-pack', (done) => {
     console.log(clc.blueBright('Starting Pushing of NuGet packages'));
 
-    let nugetDir = path.join(buildDir, '/buildoutput/nuget');
-
-    return gulp.src(nugetDir + '/*.nupkg', {read: false})
+    return gulp.src(nugetDir + '/*.nupkg', { read: false })
         .pipe(push({
             apiKey: buildSettings.NUGETAPIKEY, 
             source: 'https://api.nuget.org/v3/index.json'
@@ -161,7 +166,36 @@ gulp.task('push', gulp.series('pack', (done) => {
             done(err);
         })
         .on('end', function() { 
-            console.log(clc.blueBright('Execution of Pushing of NuGet packages completed successfully'));
+            done();
+        });
+}));
+
+gulp.task('nuget-copy-packages', (done) => {
+    console.log(clc.blueBright('Copying of Published NuGet packages'));
+
+    return gulp.src(nugetDir + '/*.nupkg')
+        .pipe(gulp.dest(nugetPublishedDir))
+        .on('error', function(err) {
+            console.log(clc.red.bold('Error during Copying of Published NuGet packages: ', err));
+            done(err);
+        })
+        .on('end', function() { 
+            console.log(clc.blueBright('Copying of Published NuGet packages completed successfully'));
+            done();
+        });
+});
+
+gulp.task('nuget-move-packages', gulp.series('nuget-copy-packages', done => {
+    console.log(clc.blueBright('Deleting of Published NuGet packages'));
+
+    return gulp.src(nugetDir + '/*.nupkg', { read: false })
+        .pipe(del())
+        .on('error', function(err) {
+            console.log(clc.red.bold('Error during Deleting of Published NuGet packages: ', err));
+            done(err);
+        })
+        .on('end', function() { 
+            console.log(clc.blueBright('Deleting of Published NuGet packages completed successfully'));
             done();
         });
 }));
@@ -174,17 +208,18 @@ gulp.task('push', gulp.series('pack', (done) => {
 
 // Build-All
 gulp.task('build-all', gulp.series('load-settings', 'clean', 'build', (done) => {
-    console.log('Version      :', buildSettings.VERSION);
-    console.log('Configuration:', buildSettings.CONFIGURATION);
     console.log("Done ...")
     done();
 }));
 
 // Publish-All
 gulp.task('publish-all', gulp.series('load-settings', 'test', 'publish-win10', (done) => {
-    console.log('Version      :', buildSettings.VERSION);
-    console.log('Configuration:', buildSettings.CONFIGURATION);
-    console.log('Build Dir    :', buildDir);
+    console.log("Done ...")
+    done();
+}));
+
+// Publish-All
+gulp.task('publish-all-nuget', gulp.series('load-settings', 'nuget-publish', 'nuget-move-packages', (done) => {
     console.log("Done ...")
     done();
 }));
